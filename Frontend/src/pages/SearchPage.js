70ed76ff30ada2d07f52c5a75d11c66c28e8ec60
Rohan Mistry <rohanmistry231@gmail.com>
2024-04-23 16:35:39 +0530

    first commit

---
+++ Frontend/src/pages/SearchPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Search from "../features/search/Search";
+
+const SearchPage = () => {
+  return (
+    <div>
+      <Search />
+    </div>
+  );
+};
+
+export default SearchPage;
